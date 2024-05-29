09c0d376db1b31f4dfa3980587782eb76db7a0bc
MHM1961 <micholaxmussa@gmail.com>
2024-05-29 03:17:24 +0300

    My first time to commit.

---
+++ datatypes.dart
@@ -0,0 +1,22 @@
+void main(){
+//declaration of variable num_1 thats storing an interger number
+int num_1 = 9;
+
+//declaration of variable num_2 thats storing an float point number
+double num_2 = 18.6;
+
+//declaration of variable fullName thats storing an name
+String fullName = "Micholax Mussa";
+
+//declaration of variable numbers thats storing an ordered collectin of interger number
+List<int> numbers = [1, 3, 5, 7, 9, 11];
+
+//declaration of variable scores thats storing an collectin of key-pair value
+Map<String, int> scores = {"Jane Sums": 98, "Thomson Juda":89, "Farida Nyuki":52, "Hokororo Jo": 34 };
+
+//dart functions for printing output
+print(num_1);
+print(num_2);print(numbers);
+print(scores);
+print("Your full name is $fullName");
+}
